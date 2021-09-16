847665b0f6bfd3d8764b4960199c9eb2764a7716
Ganesh Wagh <ganesh.onkar.wagh@hp.com>
2021-09-16 11:43:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "my_vpc" {
-  source      = "../modules/vpc"
+  source      = "./modules/vpc"
   vpc_cidr    = "192.168.0.0/16"
   tenancy     = "default"
   vpc_id      = "${module.my_vpc.vpc_id}"
@@ -11,7 +11,7 @@ module "my_vpc" {
 }
 
 module "my_ec2" {
-  source        = "../modules/ec2"
+  source        = "./modules/ec2"
   ec2_count     = 1
   ami_id        = "ami-5a8da735"
   instance_type = "t2.micro"
